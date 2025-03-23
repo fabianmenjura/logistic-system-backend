55789559ec19163c16f9b652b908a665c1f25c82
fabianmenjura <53685320+fabianmenjura@users.noreply.github.com>
2025-03-22 21:17:30 -0500

    🚧Creación de ordenes

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const authRoutes = require('./routes/authRoutes');
+import orderRoutes from './routes/orderRoutes.js';
 
 const app = express();
 
@@ -9,6 +10,7 @@ app.use(cors());
 app.use(bodyParser.json());
 
 app.use('/api/auth', authRoutes);
+app.use('/api', orderRoutes);
 
 app.use((err, req, res, next) => {
     res.status(500).json({ message: err.message });
